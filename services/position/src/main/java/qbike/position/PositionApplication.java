b7424a52bd39dc83a833a1d0ca61e4f9d6adb161
tuan <nhoklilom2.0@gmail.com>
2025-05-06 02:44:03 +0700

    connect euruka

--- services/position/src/main/java/qbike/position/PositionApplication.java
+++ services/position/src/main/java/qbike/position/PositionApplication.java
@@ -2,10 +2,12 @@ package qbike.position;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PositionApplication {
     public static void main(String[] args) {
         SpringApplication.run(PositionApplication.class,args);
